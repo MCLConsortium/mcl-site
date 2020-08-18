4ac52875bd6d6a3c44528571c624fef2fc36be4b
Sean Kelly <kelly@seankelly.biz>
2020-08-18 18:01:12 -0500

    Some initial Jenkins support for new Docker-based build

--- support/fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PATH=/usr/bin:/bin
-export PATH
-
-find /local/content/web/mcl/mcl.nci.nih.gov/var -type d -print0 | xargs -0 chmod 755
-find /local/content/web/mcl/mcl.nci.nih.gov/var -type f -print0 | xargs -0 chmod 644
